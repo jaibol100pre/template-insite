164c5ab302a7e91e3c8c5aa0e62266ec9e2fd7ea
root <root@Debian8-Desktop>
2018-10-29 15:42:56 -0400

    Se agrega slider

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # template-insite
 
-Esta es una buena plantilla (Template) para inciar un proyecto desde cero.
+Esta es una buena plantilla (Template) para inciar un proyecto desde cero, que se pretenda desarrolar sin framework, y de una manera muy ordenada.
--- web/pagina/principal.php
+++ web/pagina/principal.php
@@ -6,7 +6,8 @@
 <body>
     <div class="page">
          <div class="row"><?php include ('../include/menu.php');?> </div> <!--row-->
-         <div class="row"><?php include ('../include/iconos.php');?> </div> <!--row-->
+         <div class="row"><?php include ('../include/slider.php');?> </div> <!--row-->
+       
           <h1 class="wow bounce">Template</h1>
            
           <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quia, tempora ad est placeat numquam temporibus deserunt laborum quaerat, explicabo voluptate reiciendis eaque consequuntur nisi excepturi expedita similique, ab cum dolore?</p>
